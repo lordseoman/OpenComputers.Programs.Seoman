90fb325d3117fa2edc9343a7c815fbb8f778d339
Simon Hookway <simon@obsidian.com.au>
2014-06-27 01:22:44 +1000

    Initial testing stuff, nowhere near complete

--- tests/test_inventory.lua
+++ tests/test_inventory.lua
@@ -4,4 +4,18 @@
 
 local sFunc = require("functions")
 local Inventory = require("inventory")
+local component = require("component")
+local event = require("event")
 
+print("Please select a chest to test with.")
+local num = 1
+for addr, name in component.list() do
+  io.stdout:write("%d: %s %s", num, addr, name)
+  num = num + 1
+end
+
+local args = { event.pull("key_down") }
+local charCode = args[3]
+local code = args[4]
+
+print("You selected "..charCode.." code="..code)
