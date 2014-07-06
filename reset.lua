38cef057deacae80c71de457112bfac0810ba7c6
Simon Hookway <simon@obsidian.com.au>
2014-07-07 00:14:39 +1000

    Added reset to reset the terminal.

---
+++ reset.lua
@@ -0,0 +1,11 @@
+local component = require("component")
+local term = require("term")
+
+gpu = component.proxy(component.getPrimary("gpu")
+gpu.setForeground(0xFFFFFF)
+gpu.setBackground(0x000000)
+local x, y = gpu.maxResolution()
+gpu.setResolution(x, y)
+gpu.fill(1, 1, x, y, " ")
+
+term.setCursor(1,1)
